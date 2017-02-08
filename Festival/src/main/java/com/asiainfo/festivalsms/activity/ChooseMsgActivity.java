6e291765e8550559581fef9a0b73800e1e05585a
MicroKibaco <yangzy3@asiainfo.com>
2017-02-08 18:43:30 +0800

    添加节日短信祝福页面 add 8条节日短信 添加节日祝福布局文件

---
+++ Festival/src/main/java/com/asiainfo/festivalsms/activity/ChooseMsgActivity.java
@@ -0,0 +1,18 @@
+package com.asiainfo.festivalsms.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.asiainfo.festivalsms.R;
+
+/**
+ * 作者:小木箱 邮箱:yangzy3@asiainfo.com 创建时间:2017年02月08日18点16分 描述:
+ */
+public class ChooseMsgActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_msg);
+    }
+}
