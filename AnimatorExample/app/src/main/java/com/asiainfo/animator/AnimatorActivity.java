68ef0a781b93b5e9c9fbf300236cbc9d3121b3d0
MicroKibaco <yangzy3@asiainfo.com>
2017-02-08 16:38:38 +0800

    发送祝福短信

---
+++ AnimatorExample/app/src/main/java/com/asiainfo/animator/AnimatorActivity.java
@@ -0,0 +1,13 @@
+package com.asiainfo.animator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AnimatorActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animator);
+    }
+}
